0e8b782991f2b83fd99f3a37ded3093e22a009f5
yokoasuka29@gmail.com <yokoasuka29@gmail.com>
2025-03-26 15:24:27 -0300

    atualizado

--- api/src/router.js
+++ api/src/router.js
@@ -2,28 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const Cliente = require('./controllers/cliente');
-const Telefone = require('./controllers/telefone');
-const Pedido = require('./controllers/pedido');
 
 router.get('/',(req, res)=>{
     res.json({titulo:'SNOOPY PetSHop API'});
 });
 
-router.post('/clientes',Cliente.create);
-router.get('/clientes',Cliente.read);
-router.get('/clientes/:id',Cliente.readOne);
-router.patch('/clientes/:id',Cliente.update);
-router.delete('/clientes/:id',Cliente.remove);
-
-router.post('/telefones',Telefone.create);
-router.get('/telefones',Telefone.read);
-router.patch('/telefones/:id',Telefone.update);
-router.delete('/telefones/:id',Telefone.remove);
-
-router.post('/pedidos',Pedido.create);
-router.get('/pedidos',Pedido.read);
-router.get('/pedidos/:id',Pedido.readOne);
-router.patch('/pedidos/:id',Pedido.update);
-router.delete('/pedidos/:id',Pedido.remove);
+router.post('/cliente',Cliente.create);
+router.get('/cliente',Cliente.read);
+router.patch('/cliente/:id',Cliente.update);
+router.delete('/cliente/:id',Cliente.remove);
 
 module.exports = router;
